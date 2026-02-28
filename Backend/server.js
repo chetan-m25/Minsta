bce73d376472aa4b76772b1c26b19d56a34e75aa
chetan-m26 <chetan.mm25@gmail.com>
2026-03-01 00:33:50 +0530

    chore(server): add env var support for deployment port

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/database");
 
+const PORT = process.env.PORT || 8000;
+
 connectDB();
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000 🏃");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} 🚀`);
 });
